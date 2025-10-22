9dc61c97edf389ff7065f80e8826787474ca000b
thiennt176 <thanhthien176@gmail.com>
2025-10-22 13:46:24 +0700

    Build CreatePostView, EditPostView, DeletePostView class in views of blog's app

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("allposts/", views.AllPostsView.as_view(), name="all-posts"),
     path("categories/<slug:slug>", views.CategoryView.as_view(), name="category"),
-    path("<slug:slug>", views.DetailPostView.as_view(), name="detail-post")
+    path("<slug:slug>", views.DetailPostView.as_view(), name="detail-post"),
+    path("add/", views.CreatePostView.as_view(), name="add-post"),
+    path("<slug:slug>/edit", views.EditPostView.as_view(), name="edit-post"),
+    path("<slug:slug>/delete", views.DeletePostView.as_view(), name="delete-post")
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from django.urls import reverse, reverse_lazy
 
 from .models import Post
+from .forms import PostForm, SectionFormSet
 
 # Create your views here.
 class HomeView(ListView):
@@ -20,12 +21,63 @@ class HomeView(ListView):
         context["posts"] = post[:3]
         return context
     
+class AllPostsView(ListView):
+    model = Post
+    template_name = "blogs/list-posts.html"
+    context_object_name = "posts"
+    
 class CategoryView(ListView):
     model = Post
-    template_name = "blog/list-posts.html"
+    template_name = "blogs/list-posts.html"
     context_object_name = "posts"
     
 class DetailPostView(DetailView):
     model = Post
-    template_name = "blog/detail-post.html"
+    template_name = "blogs/detail-post.html"
     context_object_name = "post"
+    
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blogs/form-post.html"
+    success_url = reverse_lazy("add-post")
+    
+
+class EditPostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blogs/form-post.html"
+    
+    def get_success_url(self):
+        return reverse("detail-post", kwargs={'slug':self.object.slug})
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = SectionFormSet(self.request.POST, self.request.FILES, instance=self.object)
+        else:
+            context['formset'] = SectionFormSet(instance=self.object)
+        return context
+    
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            sections = formset.save(commit=False)
+            for section in sections:
+                section.post = self.object
+                section.save()
+            formset.save_existing_objects()
+            return redirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
+    
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'blogs/delete-post.html'
+    context_object_name = "post"
+    
+    def get_success_url(self):
+        return reverse_lazy("all-posts")
